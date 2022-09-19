487664917346849d18f0fae9a4b3cd9305cf713f
Sajid Mahamood <110928387+Sajid00173@users.noreply.github.com>
2022-09-20 00:07:57 +0600

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class sum {
+
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+
+        int a = input.nextInt();
+
+        System.out.println("Enter second number: ");
+
+        int b = input.nextInt();
+
+        System.out.println("Sum: "+(a+b));
+    }
+}
+
